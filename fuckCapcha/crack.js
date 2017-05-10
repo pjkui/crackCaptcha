52b52cdee58216ded146ca3ccdc92fdfd1c93766
quinnpan(潘敬奎) <quinnpan@tencent.com>
2017-05-10 14:04:04 +0800

    add files

---
+++ fuckCapcha/crack.js
@@ -0,0 +1,18 @@
+document.body.onload = function (evt) {
+    console.log('evt from crack.js');
+    console.log(evt);
+    alert(123);
+}
+document.body.addEventListener('dblclick', function (evt) {
+    //  console.log('evt from crack.js');
+    // console.log(evt);
+    // alert(1233432);
+    console.log(bigImg);
+    canvas = document.createElement('canvas');
+    document.body.appendChild(canvas);
+    var bigImg = document.getElementsByClassName('big img')[0];
+    var ctx = canvas.getContext('2d');
+    ctx.drawImage(bigImg, 0, 0,bigImg.clientWidth,bigImg.clientHeight);
+    var imgPixel = ctx.getImageData(2,2,1,1);
+    console.log(imgPixel);
+});
