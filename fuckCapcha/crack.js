6402a1eff1ceed0eae197551ec405be3ec804dfa
quinn pan <pjkui@qq.com>
2017-05-12 00:11:32 +0800

    get pictures data

--- fuckCapcha/crack.js
+++ fuckCapcha/crack.js
@@ -1,4 +1,4 @@
-document.body.onload = function (evt) {
+document.body.onload = function(evt) {
     console.log('evt from crack.js');
     console.log(evt);
     alert(1243);
@@ -9,6 +9,9 @@ var canvas = document.createElement('canvas');
 
 var slideBlockSize = 0;
 var totalBlockSize = null;
+var linePixels1 = [];
+var linePixels2 = [];
+var linePixels3 = [];
 
 document.body.appendChild(infoShowDiv);
 document.body.appendChild(canvas);
@@ -41,7 +44,7 @@ function getSlideBlockSize() {
     }
 }
 
-document.body.addEventListener('dblclick', function (evt) {
+document.body.addEventListener('dblclick', function(evt) {
     //  console.log('evt from crack.js');
     // console.log(evt);
     // alert(1233432);
@@ -56,17 +59,49 @@ document.body.addEventListener('dblclick', function (evt) {
     console.log('big Img：');
     console.log(bigImg);
 
-    canvas.style.height = '160px';
-    canvas.style.width = '280px';
+    canvas.style.height = '320px';
+    canvas.style.width = '560px';
 
     var ctx = canvas.getContext('2d');
     ctx.drawImage(bigImg, 0, 0, bigImg.naturalWidth, bigImg.naturalHeight, 0, 0, totalBlockSize.width, totalBlockSize.height);
-    var imgPixel = ctx.getImageData(2, 2, 1, 1);
-    console.log(imgPixel);
+    linePixels1.length = 0;
+    linePixels2.length = 0;
+    linePixels3.length = 0;
+    var naturalTop = slideBlockSize.top * 2
+    var processedTop1 = slideBlockSize.top + slideBlockSize.height * (2 / 7);
+    var processedTop2 = slideBlockSize.top + slideBlockSize.height / 2;
+    var processedTop3 = slideBlockSize.top + slideBlockSize.height * (5 / 7);
+    for (var x = 0; x < totalBlockSize.width; ++x) {
+        var imgPixel1 = ctx.getImageData(x, processedTop1, 1, 1);
+        var imgPixel2 = ctx.getImageData(x, processedTop2, 1, 1);
+        var imgPixel3 = ctx.getImageData(x, processedTop3, 1, 1);
+
+        linePixels1.push(imgPixel1);
+        linePixels2.push(imgPixel2);
+        linePixels3.push(imgPixel3);
+    }
+    console.log(linePixels1);
+    console.log(linePixels2);
+    console.log(linePixels3);
 
     ctx.beginPath();
-    ctx.moveTo(0,slideBlockSize.top);
-    ctx.lineTo(280,60);
+    ctx.moveTo(0, slideBlockSize.top);
+    ctx.lineTo(280, slideBlockSize.top);
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.moveTo(0, processedTop1);
+    ctx.lineTo(280, processedTop1);
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.moveTo(0, processedTop2);
+    ctx.lineTo(280, processedTop2);
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.moveTo(0, processedTop3);
+    ctx.lineTo(280, processedTop3);
     ctx.stroke();
 
     slideBlock = document.getElementById('slideBlock');
---
+++ fuckCapcha/readme.md
@@ -0,0 +1,5 @@
+##top
+relative size to native size 
+42  ==> 84
+5 ==> 10
+97 ==> 195
